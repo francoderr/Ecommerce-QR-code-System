ed30b9fd150fc542e0351e418eff66fa1d688ebd
Shedrack Okunda <okundashedrack@gmail.com>
2024-10-23 14:07:30 +0300

    added a session for storing items added to cart for the session only

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const productRoutes = require("./routes/productRoutes");
+const session = require("express-session");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(
+  session({
+    secret:
+      "8139110896719cbeffb5a0a5e9b157f1b17779579a563626dd7c2b6b92d46b3b4bc5e688f2e4b55ad4fc550f606f9f4859baa02ef1f7ef251111597066e87886",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
 
-app.use("/api", productRoutes);
+app.use("/", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Boutique backend API is running");
